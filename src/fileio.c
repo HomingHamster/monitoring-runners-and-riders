467afa594c5c604f1cd25fc00efd1835e15d3b3b
Felix Farquharson <felix.farquharson@googlemail.com>
2012-12-14 03:13:36 +0000

    check if filename is absolute.

--- src/fileio.c
+++ src/fileio.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void parse_name_file(char filename[]) {
     FILE *name_file;
+    
+    char datafile[100] = "dat/";
     char name[80], date[25], time[6];
     
-    name_file = fopen(filename, "r");
+            
+    if (filename[0] == '/'){
+        strcat(datafile, filename);
+    }
+    
+    name_file = fopen(datafile, "r");
     
     fscanf(name_file, " %79[0-9A-Za-z- ]", name);
     fscanf(name_file, " %4[0-9A-Za-z ]", date);
     fscanf(name_file, " %5[0-9A-Za-z:]", time);
     
     fclose(name_file);
+}
+
+void parse_nodes_file(char filename[]) {
+    
+}
+
+void parse_tracks_file(char filename[]) {
+    
+}
+
+void parse_courses_file(char filename[]) {
+    
+}
+
+void parse_entrants_file(char filename[]) {
+    
 }
--- src/fileio.h
+++ src/fileio.h
@@ -9,6 +9,10 @@
 #define	FILEIO_H
 
 void parse_name_file(char filename[]);
+void parse_nodes_file(char filename[]);
+void parse_tracks_file(char filename[]);
+void parse_courses_file(char filename[]);
+void parse_entrants_file(char filename[]);
 
 #endif	/* FILEIO_H */
 
